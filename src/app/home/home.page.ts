2e48dfa7e3625e7413db33924c2ac0924b99e7e1
alemupx <alemupx@gmail.com>
2021-02-15 04:38:53 -0500

    Propuesta index completada

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -13,6 +13,11 @@ export class HomePage {
 
   pics = {
     pic1: { url: "../../assets/pics/pic1.jpg" },
+    pic2: { url: "../../assets/pics/pic2.jpg" },
+    pic3: { url: "../../assets/pics/pic3.jpg" },
+    pic4: { url: "../../assets/pics/pic4.jpg" },
+    pic5: { url: "../../assets/pics/pic5.jpg" },
+    pic6: { url: "../../assets/pics/pic6.jpg" },
   };
 
   constructor() { }
